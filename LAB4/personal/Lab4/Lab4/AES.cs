11613fe074ed0d3f0529ae1ad32a8aed920638f5
Vũ Thành Công <88937753+thanhcong0111@users.noreply.github.com>
2024-06-04 19:32:03 +0700

    Add files via upload

---
+++ LAB4/personal/Lab4/Lab4/AES.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class AES
+    {
+        AesCryptoServiceProvider aess;
+
+        public AES(string key)
+        {
+            aess = new AesCryptoServiceProvider();
+            aess.KeySize = 256;
+            aess.BlockSize = 128;
+            aess.IV = new byte[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            aess.Key = System.Text.ASCIIEncoding.ASCII.GetBytes(key);
+            aess.Mode = CipherMode.CBC; //cipher block chaining
+            aess.Padding = PaddingMode.PKCS7;
+        }
+
+        public byte[] Encrypt(string plaintext)
+        {
+            ICryptoTransform trans = aess.CreateEncryptor();
+            byte[] enBytes = trans.TransformFinalBlock(ASCIIEncoding.ASCII.GetBytes(plaintext), 0, plaintext.Length);
+            return enBytes;
+        }
+
+        public string Decrypt(byte[] ciphertext)
+        {
+            byte[] deBytes;
+            try
+            {
+                ICryptoTransform trans = aess.CreateDecryptor();
+                deBytes = trans.TransformFinalBlock(ciphertext, 0, ciphertext.Length);
+            }
+            catch (CryptographicException ex)
+            {
+                // Handle the exception and return an appropriate error message
+                return $"Decryption failed: {ex.Message}";
+            }
+            catch (Exception)
+            {
+                // Handle any other exceptions and rethrow them
+                throw;
+            }
+            return ASCIIEncoding.ASCII.GetString(deBytes);
+        }
+    }
+}
